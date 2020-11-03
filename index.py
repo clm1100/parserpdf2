cac92d5d38143e485f40f15d914e6c5859d97924
陈黎明 <275298711@qq.com>
2020-11-03 19:50:11 +0800

    init

--- index.py
+++ index.py
@@ -5,13 +5,19 @@ import pdfplumber
 file_list = os.listdir('./pdf')
 for fname in file_list:
     if '.pdf' in fname:
+        # print(fname)
         p = './pdf/'+fname
         with pdfplumber.open(p) as pdf:
             page = pdf.pages[0]
             content = page.extract_text()
-            if content.find('电子普通发票')>0:
-                dealPDF1(p)
-            elif content.find('电子报销凭证序号')>0:
-                dealPDF2(p)
+            print('===============')
+            print(p)
+            print(content)
+            print('===============')
+            # if content.find('电子报销凭证序号') > 0:
+            #     print(fname)
+            #     dealPDF2(p,content)
+            # else:
+            #     dealPDF1(p,content)
 
 
--- one.py
+++ one.py
@@ -3,19 +3,15 @@ import shutil
 import re
 
 
-def dealPDF1(p):
-    with pdfplumber.open(p) as pdf:
-            page= pdf.pages[0]
-            content = page.extract_text()
-            print(content.find('电子普通发票'))
-            matchObj = re.search(r'发票代码:[0-9]+', content, re.M | re.I)
-            matchObj2 = re.search(r'发票号码:[0-9]+', content, re.M | re.I)
-            matchObj3 = re.search(r'（小写）￥[0-9]+[.]?[0-9]+', content, re.M | re.I)
-            code = matchObj.group().split(':')[1]
-            num = matchObj2.group().split(':')[1]
-            money = matchObj3.group().split('￥')[1]
-            print(code)
-            print(num)
-            print(money)
-            dist = code+'-'+num+'-'+money+'-'+'陈黎明'+'.pdf'
-            shutil.copyfile(p, dist)
+def dealPDF1(p,content):
+    matchObj = re.search(r'发票代码:[0-9]+', content, re.M | re.I)
+    matchObj2 = re.search(r'发票号码:[0-9]+', content, re.M | re.I)
+    matchObj3 = re.search(r'（小写）￥[0-9]+[.]?[0-9]+', content, re.M | re.I)
+    code = matchObj.group().split(':')[1]
+    num = matchObj2.group().split(':')[1]
+    money = matchObj3.group().split('￥')[1]
+    # print(code)
+    # print(num)
+    # print(money)
+    dist ='./pdf2/'+code+'-'+num+'-'+money+'-'+'陈黎明'+'.pdf'
+    shutil.copyfile(p, dist)
